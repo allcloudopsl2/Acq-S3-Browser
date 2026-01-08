24f19e1b04909080dccb7a7c7a5b4b2dc506d2cd
nikhil.akula@acqueon.com <allcloudopsl2@acqueon.com>
2026-01-08 20:06:36 +0530

    Updating Folder Path in permissions

--- amplify/accessConfig.ts
+++ amplify/accessConfig.ts
@@ -6,7 +6,7 @@ export interface AccessRule {
 }
 
 export const accessConfig: Record<string, AccessRule[]> = {
-  // Full CRUD access to IMServUAT folder and subfolders
+  // Full CRUD access to NewDay folder and subfolders
   Administrator: [
     // {
     //   // Grant list permission on the root to see folders
@@ -14,7 +14,7 @@ export const accessConfig: Record<string, AccessRule[]> = {
     //   permissions: ["list"],
     // },
     {
-      path: "IMServUAT/*",
+      path: "NewDay/*",
       permissions: ["get", "list", "write", "delete"],
     },
   ],
@@ -27,7 +27,7 @@ export const accessConfig: Record<string, AccessRule[]> = {
     //   permissions: ["list"],
     // },
     {
-      path: "IMServUAT/*",
+      path: "NewDay/*",
       permissions: ["get", "list", "write"],
     },
   ],
@@ -35,11 +35,11 @@ export const accessConfig: Record<string, AccessRule[]> = {
   // Limited: RW access to 2 subfolders + view root-level files.
   LimitedContributor: [
     {
-      path: "IMServUAT/PreProcAutoupload/*",
+      path: "NewDay/PreProcAutoupload/*",
       permissions: ["get", "list", "write"],
     },
     {
-      path: "IMServUAT/DataExtract/*",
+      path: "NewDay/DataExtract/*",
       permissions: ["get", "list", "write"],
     },
   ],
